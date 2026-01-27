634ec52136a377114f98f42b1f49afe0ed3f3b85
itsjyoti <jyotichaturvedi97432@gmail.com>
2026-01-27 22:28:25 +0530

    by this we count the Digits.

---
+++ CountNumber.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class CountNumber{
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter The Number");
+      int num=sc.nextInt();
+      int count=0;
+      while(num!=0){
+        num=num/10;
+        count++;
+      }  
+      System.out.println("Total Count Number is:"+count);
+      sc.close();
+    }
+}
