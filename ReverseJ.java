6c4854140dd5fbb0fc54d03073d481d6af70cd3e
itsjyoti <jyotichaturvedi97432@gmail.com>
2026-01-30 23:56:05 +0530

    Through this we can reverse string

---
+++ ReverseJ.java
@@ -0,0 +1,19 @@
+
+    
+import java.util.Scanner;
+public class ReverseJ {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the String");
+        String num=sc.next();
+        String rev="";
+        for(int i=num.length()-1;i>=0;i--){
+            rev=rev+ num.charAt(i);
+        }
+        System.out.println("Reverse of String\n"+rev);
+        sc.close();
+    }
+    
+}
+
+
